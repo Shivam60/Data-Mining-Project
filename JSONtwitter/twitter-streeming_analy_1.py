adccfed5f9e46a06ebf8173ea3db4dd9805b88ed
shivam60 <shivamchawla60@gmail.com>
2017-03-02 00:30:40 +0530

    update 3

--- JSONtwitter/twitter-streeming_analy_1.py
+++ JSONtwitter/twitter-streeming_analy_1.py
@@ -6,6 +6,9 @@ import json
 now = datetime.datetime.now()
 outfn = "twitter_user_data_%i.%i.%i.txt" % (now.month, now.day, now.year)
 
-with open(outfn) as file:
-    data=json.load(file)
-print data
+with open(outfn, 'r') as data_file:
+    json_data = data_file.read()
+
+data = json.loads(json_data)
+for i in data:
+    print i
--- JSONtwitter/twitter-streeming_pr_1.py
+++ JSONtwitter/twitter-streeming_pr_1.py
@@ -6,8 +6,10 @@ import json
 
 now = datetime.datetime.now()
 outfn = "twitter_user_data_%i.%i.%i.txt" % (now.month, now.day, now.year)
-
+json_arr=[]
+tweet_val={}
 class MyStreamer(TwythonStreamer):
+
     def on_success(self,data):
         if 'text' in data:
             tweet_val.update({'text':data['text']})
@@ -20,25 +22,15 @@ class MyStreamer(TwythonStreamer):
             tweet_val.update({'coordinates':data['coordinates']}) 
             tweet_val.update({'locatentitiesion':data['entities']}) 
             print tweet_val
-            try: 
-                with open(outfn,'a') as outfile:
-                    json_obj=json.dump(tweet_val,outfile,sort_keys='True',indent=4)
-            except IOError:
-                print('An error occured trying to read the file.')
-            except:
-                print('An error occured.')
-            finally:   
-                outfile.close()
-            tweet_val.clear()
+            json_arr.append(tweet_val.copy())
+           # print json_arr
+           # tweet_val.clear()
+            if len(json_arr)>5:
+                self.disconnect()
+
+
     def on_error(self,status_code,data):
         print status_code
-
-try:
-    open(outfn,'w')
-except IOError:
-    print('An error occured trying to read the file.')
-except:
-    print('An error occured.')
 
 APP_KEY = 'HWXEFpnCXr7hnHqm946JRAeev'
 APP_SECRET = 'hgfwxD3NEgP9rEFWr5vFR8KPR9Q9aT7jSQfOQpMfUunfPv7j6j'
@@ -50,13 +42,24 @@ woeid=2295401 #india
 tweet_val={}
 trend_array = []
 twittertrend=Twython(app_key=APP_KEY,app_secret=APP_SECRET,oauth_token=OAUTH_TOKEN,oauth_token_secret=OAUTH_TOKEN_SECRET)
-
+'''
 place = twittertrend.get_place_trends(id=woeid)
 if place:
     for trend in place[0].get('trends', []):
         trend_array.append(trend['name'])
 for i in trend_array:
-    print i
-track=raw_input("Enter Trend TO search For: ")
+      print i
+ '''
+#track=raw_input("Enter Trend To search For: ")
 stream=MyStreamer(APP_KEY,APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET,10)
-stream.statuses.filter(track=track)
+stream.statuses.filter(track='#AzaadiDoublespeak')
+print json_arr
+try:
+    with open(outfn,'w') as outfile:
+        json_obj=json.dump(json_arr,outfile,sort_keys='True',indent=4)
+except IOError:
+    print('An error occured trying to read the file.')  
+except:
+    print('An error occured.')
+finally:
+    outfile.close()
